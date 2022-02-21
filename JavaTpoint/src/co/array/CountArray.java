d4f4b446d28a327b5eb009ba6bff4bb6a3c42581
Nitish Kumar <nitisharav@gmail.com>
2022-02-21 14:59:09 +0530

    Array length

---
+++ JavaTpoint/src/co/array/CountArray.java
@@ -0,0 +1,11 @@
+package co.array;
+
+public class CountArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]=new int[] {1,2,3,4,5,5,1,9};
+System.out.println("Array length "+arr.length);
+	}
+
+}
