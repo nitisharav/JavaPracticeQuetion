baeae96c5005c0dfe15c2eae49adca2828adcb93
Nitish Kumar <nitisharav@gmail.com>
2022-02-25 16:01:53 +0530

    Replace space

---
+++ JavaTpoint/src/co/strings/ReplaceSpace.java
@@ -0,0 +1,13 @@
+package co.strings;
+
+public class ReplaceSpace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="Once in a blue moon";
+String ch="-";
+str=str.replaceAll(" ", ch);
+System.out.println("New String: "+str);
+	}
+
+}
