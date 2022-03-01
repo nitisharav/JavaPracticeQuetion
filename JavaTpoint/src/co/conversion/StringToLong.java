df53104ca327f0ec4508b82b3b794604c29ca2bd
Nitish Kumar <nitisharav@gmail.com>
2022-03-01 15:45:22 +0530

    StringToLong

---
+++ JavaTpoint/src/co/conversion/StringToLong.java
@@ -0,0 +1,19 @@
+package co.conversion;
+
+public class StringToLong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="9990449935";
+Long lo=45612378L;
+Long lon=456123778L;
+String str2= String.valueOf(lo);
+String str1=Long.toString(lon);
+
+Long l=Long.parseLong(str);
+System.out.println(l);
+System.out.println(str2);
+System.out.println(str1);
+	}
+
+}
